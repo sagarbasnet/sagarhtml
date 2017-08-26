4c25fde210e95a08760c752154b95332d04b3f77
sagarbasnet <sagarbasnet1000@gmail.com>
2017-08-26 22:31:40 +0545

    new upload

---
+++ js/new.js
@@ -0,0 +1,8 @@
+        	$(window).scroll(function() {
+			if ($(document).scrollTop() > 150) {
+			$('.navbar').addClass('shrink');
+			}
+			else {
+			$('.navbar').removeClass('shrink'); }
+			});
+
